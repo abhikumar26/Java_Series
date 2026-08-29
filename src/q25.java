322d9ef473779d3cf87824232c12893008b7db60
Abhinav <abhinavkumarprithvi7@gmail.com>
2026-08-29 11:36:39 +0530

    changes

--- src/q25.java
+++ src/q25.java
@@ -1,6 +1,18 @@
 import java.util.Scanner;
 public class q25 {
     public static void main(String[] args) {
-        
+        //25. Find and print the sum of all factors of the given number.
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number here :");
+        int n = sc.nextInt();
+
+        int sum = 0;
+        for (int i=1; i<=n; i++){
+            if (n%i == 0){
+                sum = sum + i;
+            }
+        }
+        System.out.println("The sum of number is :" + sum);
+
     }
 }
---
+++ src/q26.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class q26 {
+    public static void main(String[] args) {
+//  26. Find the HCF (Highest Common Factor) of two given numbers.
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the First number :");
+        int num1 = sc.nextInt();
+        System.out.print("Enter the Second number :");
+        int num2 = sc.nextInt();
+
+
+    }
+}
